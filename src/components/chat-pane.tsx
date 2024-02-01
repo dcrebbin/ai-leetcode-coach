a4baf9e93c2378330f207d7afa0de71c8f74b453
Devon Crebbin <devon@artvuu.group>
2024-02-01 23:05:23 +1100

    fixed chat pane issues

--- src/components/chat-pane.tsx
+++ src/components/chat-pane.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { PlayIcon, PaperAirplaneIcon, ArrowPathIcon } from "@heroicons/react/20/solid";
 
 export default function ChatPane(props: any) {
@@ -7,10 +9,10 @@ export default function ChatPane(props: any) {
         <input
           onKeyDown={(e) => {
             if (e.key == "Enter") {
-              props.sendMessage();
+              props?.sendMessage();
             }
           }}
-          ref={props.input}
+          ref={props?.input}
           placeholder="Type your message here"
           className="bg-white ml-8 m-2 w-[100%] px-3 py-2  h-9 active:border-none text-black rounded-md
     "
@@ -20,37 +22,37 @@ export default function ChatPane(props: any) {
             <ArrowPathIcon className="h-6 w-6 text-white animate-spin" />
           </div>
         ) : (
-          <button name="sendButton" className="text-3xl font-bold m-2 pr-4" onClick={props.sendMessage}>
+          <button name="sendButton" className="text-3xl font-bold m-2 pr-4" onClick={props?.sendMessage}>
             <PaperAirplaneIcon className="h-8 w-8 text-white" />
           </button>
         )}
       </div>
-      {props.content.length === 0 ? <hr></hr> : <div className="hidden"></div>}
+      {props?.content?.length === 0 ? <hr></hr> : <div className="hidden"></div>}
       <div className="flex flex-col-reverse">
-        {props.content?.map((e: any, i: number) => {
+        {props?.content?.map((e: any, i: number) => {
           return (
             <div key={i} className="flex justify-start items-center mx-4">
               {e.role == "user" ? (
-                <div className="w-[100%] m-2 bg-[#404045] px-4 py-2  text-white">
+                <div className="min-w-full m-2 bg-slate-400 px-4 py-2  text-white">
                   <strong>You</strong>
                   <br></br>
                   {e.content}
                 </div>
               ) : (
                 <div className="flex items-center">
-                  <div className="w-[100%] m-2 bg-[#3C3D63] px-4 py-2 text-white">
+                  <div className="w-full m-2 bg-blue-800 px-4 py-2 text-white rounded-lg">
                     <strong>John Smith</strong>
                     <br></br>
                     {e.content}
                   </div>
-                  {props.speechToTextLoading ? (
+                  {props?.speechToTextLoading ? (
                     <div className="flex items-center">
                       <ArrowPathIcon className="h-6 w-6 text-white animate-spin" />
                     </div>
                   ) : (
                     <PlayIcon
                       onClick={async () => {
-                        await props.textToSpeech(e.content);
+                        await props?.textToSpeech(e?.content);
                       }}
                       className="h-10 w-10 text-white cursor-pointer"
                     ></PlayIcon>
