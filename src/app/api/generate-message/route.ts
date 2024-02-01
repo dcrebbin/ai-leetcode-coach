3addf89f97d412f57c12a7ec1bf59da1a02d66f1
Devon Crebbin <devon@artvuu.group>
2024-02-01 23:05:12 +1100

    fixed generate message api route

--- src/app/api/generate-message/route.ts
+++ src/app/api/generate-message/route.ts
@@ -1,32 +1,27 @@
 import { MessageSchema } from "@/app/page";
-import type { NextApiRequest, NextApiResponse } from "next";
 const apiKey = process.env.OPEN_AI_API_KEY;
 
-export async function POST(req: NextApiRequest, res: NextApiResponse<any>) {
-  const requestBody = JSON.parse(req.body);
-  console.log(requestBody);
-  const messages: MessageSchema[] = requestBody.messages;
-  const body: any = {
-    model: "gpt-4-turbo",
-    messages: [
-      {
-        role: "system",
-        content: "Hello, I am a chatbot created by OpenAI. How can I help you today?",
-      },
-      {
-        role: "user",
-        content: messages[messages.length - 1].content,
-      },
-    ],
-  };
+export async function POST(req: Request, res: Response) {
+  const json = await req.json();
+  const messages: MessageSchema[] = json.messages;
+
   const headers: any = {
-    ContentType: "application/json",
+    "Content-Type": "application/json",
     Authorization: `Bearer ${apiKey}`,
   };
+  const body = JSON.stringify({
+    model: "gpt-4-turbo-preview",
+    messages: [...messages],
+  });
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: headers,
     body: body,
     method: "POST",
   });
-  res.status(200).json(response);
+  const data = await response.json();
+  const generatedMessage: MessageSchema = {
+    role: "assistant",
+    content: data.choices[0].message.content,
+  };
+  return new Response(JSON.stringify(generatedMessage));
 }
